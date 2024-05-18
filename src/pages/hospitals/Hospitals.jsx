209eb2cc8492dc7ff73187e4b3d460c4bd650bf7
PALLAB ROY <76547199+PallabRoy-SE@users.noreply.github.com>
2024-05-18 23:44:09 +0530

    refactor: add hospital details component ( refs #3  )

--- src/pages/hospitals/Hospitals.jsx
+++ src/pages/hospitals/Hospitals.jsx
@@ -1,24 +1,23 @@
-import { Box, Button, Container, Typography } from "@mui/material";
-import React, { useEffect, useState } from "react";
-import MedHeader from "../../components/header/MedHeader";
-import MedCardContainer from "../../components/card-container/MedCardContainer";
-import MedSearch from "../../components/search/MedSearch";
-import useGeneric from "../../hooks/genericHook";
-import { useSearchParams } from "react-router-dom";
-import { CheckCircleOutlineOutlined, ThumbUp } from "@mui/icons-material";
-import { colors } from "../../theme/variables";
-import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
-import leftBanner from "../../assets/left_banner.png";
-import hospitalLogo from "../../assets/hospital.png";
-import MedButton from "../../components/button/MedButton";
-import { getHospitals } from "../../services/searchService";
+import { Box, Container, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import MedHeader from '../../components/header/MedHeader';
+import MedCardContainer from '../../components/card-container/MedCardContainer';
+import MedSearch from '../../components/search/MedSearch';
+import useGeneric from '../../hooks/genericHook';
+import { useSearchParams } from 'react-router-dom';
+import { CheckCircleOutlineOutlined } from '@mui/icons-material';
+import { colors } from '../../theme/variables';
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
+import leftBanner from '../../assets/left_banner.png';
+import { getHospitals } from '../../services/searchService';
+import MedHospitalDetails from '../../components/hospital-details/MedHospitalDetails';
 
 function Hospitals() {
   const { states } = useGeneric();
   const [searchParams] = useSearchParams();
   const [hospitals, setHospitals] = useState([]);
-  const [state, setState] = useState(searchParams.get("state"));
-  const [city, setCity] = useState(searchParams.get("city"));
+  const [state, setState] = useState(searchParams.get('state'));
+  const [city, setCity] = useState(searchParams.get('city'));
 
   const loadHospitals = async (state, city) => {
     try {
@@ -40,18 +39,18 @@ function Hospitals() {
   }, []);
 
   return (
-    <Box component="section">
+    <Box component='section'>
       <MedHeader />
       <Container
-        maxWidth="lg"
+        maxWidth='lg'
         sx={{
-          marginTop: "-3rem",
+          marginTop: '-3rem',
         }}
       >
         <MedCardContainer>
           <MedSearch
             sx={{
-              padding: "1.5rem",
+              padding: '1.5rem',
             }}
             states={states}
             state={state}
@@ -61,39 +60,36 @@ function Hospitals() {
         </MedCardContainer>
       </Container>
       <Container
-        maxWidth="lg"
+        maxWidth='lg'
         sx={{
-          marginBlock: "5rem",
+          marginBlock: '5rem',
         }}
       >
         <Box
-          component="div"
+          component='div'
           sx={{
-            paddingInline: "1rem",
-            marginBottom: "1.5rem",
+            paddingInline: '1rem',
+            marginBottom: '1.5rem',
           }}
         >
           <Typography
-            component="p"
+            component='p'
             sx={{
               fontWeight: 500,
-              fontSize: "1.5rem",
+              fontSize: '1.5rem',
             }}
           >
             {hospitals.length} medical centers available in {city}
           </Typography>
-          <Typography
-            component="p"
-            sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}
-          >
+          <Typography component='p' sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
             <CheckCircleOutlineOutlined
               sx={{
                 color: colors.textGrey,
-                marginRight: "1.5rem",
+                marginRight: '1.5rem',
               }}
             />
             <Typography
-              component="span"
+              component='span'
               sx={{
                 color: colors.textGrey,
               }}
@@ -105,186 +101,32 @@ function Hospitals() {
         <Grid2 container spacing={3}>
           <Grid2 md={8}>
             {hospitals.map((hospital) => (
-              <Box
-                key={hospital["Provider ID"]}
-                component="div"
-                sx={{
-                  "&:not(last-child)": {
-                    marginBottom: "1.5rem",
-                  },
-                }}
-              >
-                <MedCardContainer
-                  elevate={false}
-                  sx={{
-                    padding: "1.5rem",
-                  }}
-                >
-                  <Grid2 container spacing={2} mt={2}>
-                    <Grid2
-                      md={2.5}
-                      sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        transform: "translate(0, -2.5rem)",
-                      }}
-                    >
-                      <Box
-                        component="div"
-                        sx={{
-                          width: "7rem",
-                          height: "7rem",
-                        }}
-                      >
-                        <img
-                          src={hospitalLogo}
-                          alt="hospital"
-                          style={{
-                            width: "100%",
-                            height: "100%",
-                          }}
-                        />
-                      </Box>
-                    </Grid2>
-                    <Grid2 md={5.5}>
-                      <Typography
-                        component="p"
-                        fontWeight={600}
-                        fontSize="1.25rem"
-                        color={colors.skyBlue}
-                      >
-                        {hospital["Hospital Name"]}
-                      </Typography>
-                      <Typography
-                        component="p"
-                        fontWeight={700}
-                        fontSize="0.875rem"
-                        color={colors.smokeDark}
-                        mt={1}
-                      >
-                        {hospital.City}, {hospital.State},{" "}
-                        {hospital["ZIP Code"]}
-                      </Typography>
-                      <Typography
-                        component="p"
-                        fontSize="0.875rem"
-                        color={colors.smokeDark}
-                      >
-                        {hospital["Hospital Type"]}
-                      </Typography>
-                      <Typography
-                        component="p"
-                        fontSize="0.875rem"
-                        color={colors.smokeDark}
-                        mt={0.5}
-                        pb={1.5}
-                        borderBottom="1px dashed"
-                        borderColor={colors.borderGrey}
-                      >
-                        <Typography
-                          component="span"
-                          color={colors.textDeepGreen}
-                          fontWeight={700}
-                        >
-                          FREE
-                        </Typography>{" "}
-                        <Typography
-                          component="span"
-                          color={colors.textGrey}
-                          sx={{
-                            textDecoration: "line-through",
-                          }}
-                        >
-                          ₹500
-                        </Typography>{" "}
-                        Consultation fee at clinic
-                      </Typography>
-                      <Box component="div" mt={2}>
-                        <MedButton
-                          sx={{
-                            backgroundColor: colors.green,
-                            padding: 0,
-                            minWidth: "40px",
-                            borderRadius: "3.5px",
-                          }}
-                          size="small"
-                        >
-                          <ThumbUp
-                            sx={{ marginRight: "0.2rem", fontSize: "1rem" }}
-                          />{" "}
-                          {hospital["Hospital overall rating"] !==
-                          "Not Available"
-                            ? hospital["Hospital overall rating"]
-                            : 0}
-                        </MedButton>
-                      </Box>
-                    </Grid2>
-                    <Grid2
-                      md={4}
-                      sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "space-between",
-                        flexDirection: "column",
-                      }}
-                    >
-                      <Box
-                        component="div"
-                        sx={{
-                          display: "flex",
-                          alignItems: "center",
-                          justifyContent: "end",
-                          flexDirection: "row",
-                          height: "1rem",
-                          gap: "4%",
-                          width: "100%",
-                        }}
-                      >
-                        <Button variant="outlined" size="small" color="primary">
-                          10:30 AM
-                        </Button>
-                        <Button variant="outlined" size="small" color="success">
-                          20 April 2024
-                        </Button>
-                      </Box>
-                      <Box component="div" width="100%">
-                        <Typography
-                          component="p"
-                          align="center"
-                          color={colors.textGreen}
-                          fontSize="0.875rem"
-                          mb={1}
-                        >
-                          Available Today
-                        </Typography>
-                        <MedButton
-                          sx={{
-                            width: "100%",
-                          }}
-                        >
-                          Book FREE Center Visit
-                        </MedButton>
-                      </Box>
-                    </Grid2>
-                  </Grid2>
-                </MedCardContainer>
-              </Box>
+              <MedHospitalDetails
+                id={hospital['Provider ID']}
+                title={hospital['Hospital Name']}
+                state={hospital.State}
+                city={hospital.City}
+                zipCode={hospital['ZIP Code']}
+                type={hospital['Hospital Type']}
+                likes={
+                  hospital['Hospital overall rating'] !== 'Not Available' ? hospital['Hospital overall rating'] : 0
+                }
+              />
             ))}
           </Grid2>
           <Grid2 md={4}>
             <Box
-              component="div"
+              component='div'
               sx={{
-                height: "15rem",
+                height: '15rem',
               }}
             >
               <MedCardContainer
                 elevate={false}
                 sx={{
-                  backgroundColor: "transparent",
+                  backgroundColor: 'transparent',
                   background: `url(${leftBanner}) no-repeat 1.5% center`,
-                  backgroundSize: "225%",
+                  backgroundSize: '225%',
                 }}
               ></MedCardContainer>
             </Box>
