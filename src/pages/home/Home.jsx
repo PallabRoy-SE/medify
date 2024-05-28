bb34591b86979715f898c4eb9e709e31f7c5e1b5
PALLAB ROY <76547199+PallabRoy-SE@users.noreply.github.com>
2024-05-28 21:42:10 +0530

    feat: add offer carousel ( refs #10 )

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,8 @@ import { Box, Container } from "@mui/material";
 import MedCardContainer from "../../components/card-container/MedCardContainer";
 import MedSearchCard from "../../components/search-card/MedSearchCard";
 import useGeneric from "../../hooks/genericHook";
+import MedOfferCarousel from "../../components/offer-carousel/MedOfferCarousel";
+import { colors } from "../../theme/variables";
 
 function Home() {
   const { states } = useGeneric();
@@ -17,12 +19,26 @@ function Home() {
           position: "relative",
           marginTop: "-6rem",
           pb: 4,
+          zIndex: 1,
         }}
       >
         <MedCardContainer sx={{ pb: 4 }}>
           <MedSearchCard states={states} />
         </MedCardContainer>
       </Container>
+      <Box
+        component="section"
+        px={8}
+        sx={{
+          backgroundColor: colors.white,
+          position: "relative",
+          paddingTop: "8rem",
+          paddingBottom: "4rem",
+          marginTop: "-6rem",
+        }}
+      >
+        <MedOfferCarousel />
+      </Box>
     </Box>
   );
 }
